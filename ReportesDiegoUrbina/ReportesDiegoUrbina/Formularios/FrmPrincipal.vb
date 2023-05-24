c894c7ca084884661d72de8625a41deccb083fbf
Diego Urbina <diego.urbina14774@est.uca.edu.ni>
2023-05-24 16:37:13 -0600

    Se agrego el FrmVistaPrevia y FrmPrincipal

---
+++ ReportesDiegoUrbina/ReportesDiegoUrbina/Formularios/FrmPrincipal.vb
@@ -0,0 +1,9 @@
+﻿Public Class FrmPrincipal
+    Sub VerReporte(ByVal t As DataTable, ds As String, ByVal nombreRpt As String)
+        Try
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
